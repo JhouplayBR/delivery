b6dafd921e94b32e20811ffee57d3068e5406a04
JHONATA GUSTAVO RODRIGUES <87393280+JhouplayBR@users.noreply.github.com>
2022-05-12 22:09:50 -0300

    Add files via upload

---
+++ main.php
@@ -0,0 +1,36 @@
+
+<div class="back__img"></div>
+<div class="header-container center">
+    <div class="header">
+        <div class="header__top">
+        <div class="logo"><img src="public/img/Logomark.png"></div>
+            <div class="contc">
+                <a class="btn" href="/">Contato</a>
+            </div>
+        <div class="uls">
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="<?php echo INCLUDE_PATH?>catalogo">Catálogo</a></li>
+            </ul>
+        </div>
+        </div>
+        <div class="clean"></div>
+        <div class="center__header">
+            <div class="info__text">
+                <h1>Chame por Comida <q>Delivery</q> </h1><br><br>
+                <div class="btnscell">
+                    <a href="#">Baixar Aplicativo</a>
+                    <a href="<?php echo INCLUDE_PATH?>catalogo">Entrar pela Web</a>
+                </div>
+
+            </div>
+        <div class="png__guy">
+            <img src="public/img/pizza.png">
+        </div>
+        </div>
+    </div>
+
+</div>
+
+
